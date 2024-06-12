6881f435ce58ad4f14c9b4d7918935ca267c88ca
JonathanPicques <jonathan.picques@gmail.com>
2024-06-12 15:33:09 +0200

    feat: add supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
 import { Canvas } from "@react-three/fiber";
 import { Float, OrbitControls } from "@react-three/drei";
 import { ReactLogo } from "./ReactLogo";
+import { supabase } from "./main";
+import { useEffect, useState } from "react";
 
 function App() {
-  console.log(import.meta.env);
+  const [lovers, setLovers] = useState<string[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const result = await supabase.from("react_lovers").select("github_username");
+      if (!result.data) return;
+      setLovers(result.data.map((d) => d.github_username as string));
+    })();
+  }, []);
+
+  useEffect(() => {
+    const subscription = supabase
+      //
+      .channel("table-db-changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          table: "react_lovers",
+          schema: "public",
+        },
+        (payload) => {
+          console.log("Event received!", payload);
+        }
+      );
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
   return (
     <>
+      {lovers.map((lover) => (
+        <div key={lover}>{lover}</div>
+      ))}
       <Canvas>
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App.tsx";
 import "./index.css";
+
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
